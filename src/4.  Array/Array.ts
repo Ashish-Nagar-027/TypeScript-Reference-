59745bcffa9e3c7038111f7cc324baa7065a0570
Ashish Nagar <Ashishnagar033@gmail.com>
2023-10-16 07:54:51 +0530

    notes update

--- src/4.  Array/Array.ts
+++ src/4.  Array/Array.ts
@@ -1,16 +1,33 @@
+
+//Array 
+
+let newNames = ["john", "jane", "tom"] 
+// this is string array , you can only push string in this
+
+//newNames.push(3) // error
+newNames.push('jake')  //valid
+
+let newNumbers = [11,22,12] 
+// this is number array you can only push numbers in it
+newNumbers.push(32)
+// newNumbers.push(true)  // error
+
+
 /* 
+===========================
+ Defining array type
+
 Array can typed using a type annotation followed by empty array brackets, like number[]
 for an Array of numbers
+============================
 */
+
 
 // empty array
 let emptyArray: [] = []
 
-
-
 // any array
 let newArray: any[] = []
-
 
 // string Array 
 let names: string[] = ['hello', 'world'];
@@ -18,19 +35,28 @@ let names: string[] = ['hello', 'world'];
 // number array
 let ages: number[] = [122,233,4563,313]
 
-
-// 
+// boolean array
 const bools: boolean[] = []
+bools.push(true)
+
+// ==========
+// if you  want to push multiple type on same array
+// then you can use union type (next chapter)
+// unioun type
+//============
+let testStringOrNumberArr: (string | number)[]
+testStringOrNumberArr = [1, 'two' , 3]
+
+
 
 
 
 // AlterNative Syntax
 let Myname: Array <string> = ['hello', 'world' ]
-
 let myAge: Array <number> = [12,34,454,546]
 
-//note : These array only allow data of that one type inside them
-
+//note : These array only allow 
+// data of that one type inside them
 
 /*
 ================================
@@ -38,9 +64,9 @@ let myAge: Array <number> = [12,34,454,546]
 ================================
  */
 
-const activeUser: [] = []                // this is empty array i can not put values in it
-
-
+const activeUser: [] = []                
+// this is empty array i can not put values in it
+// activeUser.push('sta') // error
 
 /*
 ================================
@@ -57,7 +83,6 @@ coords.push({x: 23, y: 8})
 
 
 
-
 /*
 ================================
      multidimensial Array
@@ -68,3 +93,5 @@ coords.push({x: 23, y: 8})
 const board: string[][] = [ ["X", "O", "X"],["X", "O", "X"], ["X", "O", "X"] ]
 
 //  we can also create more dimensial array if we need
+
+
